16aa66cb08253334213694cd4c5ca3bf49d495a6
Ash <ash.zsh@proton.me>
2022-11-02 20:32:32 -0500

    max cpu workload dedicate each thread to a subset of files, rather than start/end parallel jobs per file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use num::Complex;
 use palette::{convert::TryFromColor, Hsv, RgbHue, Srgb};
 use rayon::prelude::*;
-use std::{borrow::Cow, fs::File, io::Write};
+use std::{
+    borrow::Cow,
+    fs::File,
+    io::{self, Write},
+};
 
 const X_RES: usize = 2000;
 const Y_RES: usize = 2000;
@@ -9,74 +13,112 @@ const N: usize = 2000;
 const HEADER: &'static str = const_format::formatcp!("P3\n{X_RES} {Y_RES}\n255\n");
 const OUT_PREFIX: &'static str = "out/out";
 
-fn main() -> std::io::Result<()> {
-    let mut buf_ppm = HEADER.to_string();
-    let mut buf_fname = OUT_PREFIX.to_string();
+fn main() -> io::Result<()> {
+    let thread_count = rayon::current_num_threads();
+    let iters = 1..N + 1;
+    let progress = {
+        let bar = indicatif::ProgressBar::new(N as u64);
 
-    for max_iterations in 1..N + 1 {
-        let mut file = {
-            buf_fname.push_str(format!("{max_iterations:04}").as_str());
-            buf_fname.push_str(".ppm");
-            let f = File::create(&buf_fname)?;
-            buf_fname.drain(OUT_PREFIX.len()..);
+        let n_width = (N as f64).log10() as usize;
 
-            f
-        };
-
-        let s = (-(max_iterations as f64) / 50.0).exp();
-        let scaler = scaler(
-            X_RES,
-            Y_RES,
-            -1.75 * s + 0.1000001009999,
-            1.75 * s + 0.1000001009999,
-            -1.75 * s + 0.0999989899,
-            1.75 * s + 0.0999989899,
+        bar.set_style(
+        indicatif::ProgressStyle::default_bar()
+            .progress_chars("#=-")
+            .template(format!("[{{elapsed_precise}}] [{{bar:40.cyan/blue}}] {{pos:>{n_width}}}/{{len:{n_width}}} {{msg}}").as_str())
+            .expect("bad progress bar style")
         );
 
-        let pixels: Vec<_> = (0..Y_RES)
-            .into_par_iter()
-            .flat_map(|y| (0..X_RES).into_par_iter().map(move |x| [x, y]))
-            .map(|point| {
-                let scaled = scaler(point);
+        bar
+    };
 
-                julia(
-                    scaled,
-                    |p: Complex<f64>| {
-                        let c = Complex::new(-0.8, 0.156);
+    let result: Result<Vec<()>, _> = (0..thread_count)
+        .into_par_iter()
+        .map(|iter_offset| iters.clone().skip(iter_offset).step_by(thread_count))
+        .map(|iters_thread| -> io::Result<()> {
+            let mut buf_ppm = HEADER.to_string();
+            let mut buf_fname = OUT_PREFIX.to_string();
 
-                        p.powu(2) + c
-                    },
-                    max_iterations,
-                    3.0,
-                )
-                .map_or(Cow::Borrowed("0 0 0\n"), |iterations| {
-                    let coef = (iterations as f64) / (max_iterations as f64);
+            for max_iters in iters_thread {
+                let mut file = {
+                    buf_fname.push_str(format!("{max_iters:04}").as_str());
+                    buf_fname.push_str(".ppm");
+                    let f = File::create(&buf_fname)?;
+                    buf_fname.drain(OUT_PREFIX.len()..);
 
-                    let hsv = Hsv::new(RgbHue::from_degrees(360.0 * coef - 180.0), 1.0, 1.0);
+                    f
+                };
 
-                    let Srgb {
-                        red, green, blue, ..
-                    } = Srgb::try_from_color(hsv).unwrap();
-                    let [red, green, blue] = [red, green, blue].map(|v| (v * 255.0) as u8);
+                let s = (-(max_iters as f64) / 50.0).exp();
+                let scaler = scaler(
+                    X_RES,
+                    Y_RES,
+                    -1.75 * s + 0.1000001009999,
+                    1.75 * s + 0.1000001009999,
+                    -1.75 * s + 0.0999989899,
+                    1.75 * s + 0.0999989899,
+                );
 
-                    Cow::Owned(format!("{red} {green} {blue}\n"))
-                })
-            })
-            .collect();
+                let pixels: Vec<_> = (0..Y_RES)
+                    .into_par_iter()
+                    .flat_map(|y| (0..X_RES).into_par_iter().map(move |x| [x, y]))
+                    .map(|point| {
+                        let scaled = scaler(point);
 
-        {
-            for pixel in pixels {
-                buf_ppm.push_str(pixel.as_ref())
+                        julia(
+                            scaled,
+                            |p: Complex<f64>| {
+                                let c = Complex::new(-0.8, 0.156);
+
+                                p.powu(2) + c
+                            },
+                            max_iters,
+                            3.0,
+                        )
+                        .map_or(Cow::Borrowed("0 0 0\n"), |iterations| {
+                            let coef = (iterations as f64) / (max_iters as f64);
+
+                            let hsv =
+                                Hsv::new(RgbHue::from_degrees(360.0 * coef - 180.0), 1.0, 1.0);
+
+                            let Srgb {
+                                red, green, blue, ..
+                            } = Srgb::try_from_color(hsv).unwrap();
+                            let [red, green, blue] = [red, green, blue].map(|v| (v * 255.0) as u8);
+
+                            Cow::Owned(format!("{red} {green} {blue}\n"))
+                        })
+                    })
+                    .collect();
+
+                {
+                    for pixel in pixels {
+                        buf_ppm.push_str(pixel.as_ref())
+                    }
+
+                    file.write_all(buf_ppm.as_bytes())?;
+
+                    buf_ppm.drain(HEADER.len()..);
+                }
+
+                progress.inc(1);
             }
 
-            file.write_all(buf_ppm.as_bytes())?;
+            Ok(())
+        })
+        .collect();
 
-            buf_ppm.drain(HEADER.len()..);
+    match result {
+        Ok(_) => {
+            progress.finish();
+
+            Ok(())
         }
+        Err(e) => {
+            progress.abandon();
 
-        eprint!("\rWrote image #{:03}", max_iterations);
+            Err(e)
+        }
     }
-    Ok(())
 }
 
 fn scaler(
